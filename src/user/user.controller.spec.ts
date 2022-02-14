7459008b5bab48758df76355217020bbb37c34f6
vaskania <vnikabadze@unisens.ge>
2022-02-14 20:37:49 +0400

    unit test start

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,18 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CreateUserDTO } from './dto/create-user.dto';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { AuthService } from '../auth/auth.service';
+
+const data = {
+  username: 'vaskania',
+  firstname: 'vasili',
+  lastname: 'nikabadze',
+  password: '123456',
+};
 
 describe('UserController', () => {
   let controller: UserController;
 
   beforeEach(async () => {
-	const module: TestingModule = await Test.createTestingModule({
-		controllers: [UserController],
-	}).compile();
+    const fakeUserService: Partial<UserService> = {
+      createUser: (createUserDto: CreateUserDTO) => {
+        return Promise.resolve({ username: createUserDto.username });
+      },
+    };
 
-	controller = module.get<UserController>(UserController);
+    const fakeAuthService: Partial<AuthService> = {};
+
+    const module = await Test.createTestingModule({
+      controllers: [UserController],
+      providers: [
+        { provide: UserService, useValue: fakeUserService },
+        { provide: AuthService, useValue: fakeAuthService },
+      ],
+    }).compile();
+
+    controller = module.get<UserController>(UserController);
   });
 
-  it('should be defined', () => {
-	expect(controller).toBeDefined();
+  it('create new user', async () => {
+    const userCreated = await controller.createUser(data);
+    expect(userCreated).toBeDefined();
   });
 });
